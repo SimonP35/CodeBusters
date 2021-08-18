2abe12806e9f775ac34b00d7c8850dc849f01e3e
Geoffrey-Dct <geoffreydecobert.gd@gmail.com>
2021-08-18 15:47:04 +0200

    feat-component-header

--- src/components/App/index.js
+++ src/components/App/index.js
@@ -2,14 +2,13 @@
 import React from 'react';
 
 // == Import
-import reactLogo from './react-logo.svg';
+import Header from 'src/components/Header';
 import './styles.css';
 
 // == Composant
 const App = () => (
   <div className="app">
-    <img src={reactLogo} alt="react logo" />
-    <h1>Composant : App</h1>
+    <Header />
   </div>
 );
 
