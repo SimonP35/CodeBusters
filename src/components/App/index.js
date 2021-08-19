a7ecdef359566155b455cf7ecfdba2b0a1315fee
ValtentinVW <valentinvanweydevelt@hotmail.fr>
2021-08-19 12:39:24 +0200

    refactor-remove-footer-callFooterInApp

--- src/components/App/index.js
+++ src/components/App/index.js
@@ -3,16 +3,12 @@ import React from 'react';
 
 // == Import
 import Header from 'src/components/Header';
-import Footer from 'src/components/Footer';
-
-
 import './styles.css';
 
 // == Composant
 const App = () => (
   <div className="app">
     <Header />
-    <Footer />
   </div>
 );
 
