368f3579bb182b58e0e4d8789bef8a567ba8778e
Geoffrey-Dct <geoffreydecobert.gd@gmail.com>
2021-08-19 11:12:23 +0200

    feat-mise en place du composant

---
+++ src/components/NotFound/index.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+import './notFound.scss';
+
+const NotFound = () => (
+  <div className="notFound-container">
+    <p>404</p>
+  </div>
+);
+
+export default NotFound;
