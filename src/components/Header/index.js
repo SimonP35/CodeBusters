61f6926855f7164e6eb42252beddfbbc07016622
Elenaaab <ellelaby@gmail.com>
2021-08-19 21:12:32 +0200

    update-component-home-css-component-popup-content

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -3,11 +3,13 @@ import React from 'react';
 import Nav from 'src/components/Nav';
 import Title from './Title';
 import Logo from './Logo';
+import ButtonLog from './ButtonLog';
 
 const Header = () => (
   <header className="header-container">
     <Title />
     <Logo />
+    <ButtonLog />
     <Nav />
   </header>
 );
--- src/components/Home/index.js
+++ src/components/Home/index.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import Header from 'src/components/Header';
+import Footer from 'src/components/Footer';
 
 import games from 'src/assets/images/games.png';
 import utils from 'src/utils';
@@ -19,10 +20,13 @@ const Home = () => (
         {scenarios.map((scenario) => (
           <div className="popular-scenarios-scenario" key={scenario.id}>
             <img className="popular-scenarios-scenario-image" src={scenario.image} alt="scenario-img" />
+            <p className="popular-scenarios-scenario-text">Viens donc te gâcher le week-end à débugger le code dégueulasse de tonton Maurice !
+            </p>
           </div>
         ))}
       </div>
     </main>
+    <Footer />
   </>
 );
 
