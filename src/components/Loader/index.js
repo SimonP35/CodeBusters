2067cdea14692b7a7789b3a914db9fe77f934727
Elenaaab <ellelaby@gmail.com>
2021-08-19 15:10:28 +0200

    update-nav-router-navlink

---
+++ src/components/Loader/index.js
@@ -0,0 +1,13 @@
+// == Import npm
+import React from 'react';
+
+// == Import
+import './loader.scss';
+
+// == Composant
+const Loader = () => (
+  <div className="loader" />
+);
+
+// == Export
+export default Loader;
--- src/components/Nav/index.js
+++ src/components/Nav/index.js
@@ -1,60 +1,56 @@
 import React from 'react';
-// import { NavLink } from 'react-router-dom';
+import { NavLink } from 'react-router-dom';
 
-// import Home from 'src/components/Home';
-// import About from 'src/components/About';
-// import Scenarios from 'src/components/Scenarios';
-// import Profil from 'src/components/Profil';
 
 import './nav.scss';
 
 const Nav = () => (
-  <div className="nav-container">
-    <span className="nav-link">Accueil</span>
-    <span className="nav-link">A propos</span>
-    <span className="nav-link">Scénarios</span>
-    <span className="nav-link">Profil</span>
-  </div>
+  <nav className="nav-container">
+    <NavLink
+      exact
+      to="/"
+      className="nav-link"
+      activeClassName="nav-link-active"
+    >
+      Accueil
+    </NavLink>
+    <NavLink
+      exact
+      to="/about"
+      className="nav-link"
+      activeClassName="nav-link-active"
+    >
+      A propos
+    </NavLink>
+    <NavLink
+      exact
+      to="/scenarios"
+      className="nav-link"
+      activeClassName="nav-link-active"
+
+    >
+      Scénarios
+    </NavLink>
+    <NavLink
+      exact
+      to="/profil"
+      className="nav-link"
+      activeClassName="nav-link-active"
+    >
+      Profil
+    </NavLink>
+  </nav>
 );
 
-// Nav.propTypes = 
+// Nav.propTypes =
 
 export default Nav;
 
 // const Nav = () => (
-//   <nav className="nav-container">
-//     <NavLink
-//       to="/"
-//       className="nav-link"
-//       activeClassName="nav-item--active"
-//       exact
-//     >
-//       Accueil
-//     </NavLink>
-//     <NavLink
-//       to="/about"
-//       className="nav-link"
-//       activeClassName="nav-item--active"
-//       exact
-//     >
-//       A propos
-//     </NavLink>
-//     <NavLink
-//       to="/scenarios"
-//       className="nav-link"
-//       activeClassName="nav-item--active"
-//       exact
-//     >
-//       Scénarios
-//     </NavLink>
-//     <NavLink
-//       to="/profil"
-//       className="nav-link"
-//       activeClassName="nav-item--active"
-//       exact
-//     >
-//       Profil
-//     </NavLink>
-//   </nav>
+//   <div className="nav-container">
+//     <span className="nav-link">Accueil</span>
+//     <span className="nav-link">A propos</span>
+//     <span className="nav-link">Scénarios</span>
+//     <span className="nav-link">Profil</span>
+//   </div>
 // );
-
