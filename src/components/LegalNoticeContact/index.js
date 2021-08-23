93d3dbf9bd41125b969d7409e7124f8b88def3e9
ValtentinVW <valentinvanweydevelt@hotmail.fr>
2021-08-23 09:28:52 +0200

    create-update-legalnotice

--- src/components/LegalNoticeContact/index.js
+++ src/components/LegalNoticeContact/index.js
@@ -1,10 +1,14 @@
 // == Import npm
 import React from 'react';
-import './noticeConstact.scss';
+import './noticeContact.scss';
 
 // == Composant
 const LegalNoticeContact = () => (
   <div className="notice-container">
+    <h2 className="notice-title">Politique sur les Cookies</h2>
+      <p className="notice-paragraph">Les cookies serviont à vous identifier</p>
+    <h2 className="notice-title">Données personnelles</h2>
+      <p className="notice-paragraph">CodeBusters est un projet de fin de formation vos données personnelles seront stockée en base de donnée pour vous identifier</p>
   </div>
 );
 
