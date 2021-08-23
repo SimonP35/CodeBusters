1689eb4badd64c5f2f1cb0a412fa3404d18a67d6
Geoffrey-Dct <geoffreydecobert.gd@gmail.com>
2021-08-23 21:39:38 +0200

    create component Game with switch on route and subComponent intro+board+win+loose

---
+++ src/components/Game/Intro/index.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+import './intro.scss';
+
+const Intro = () => (
+  <h2 className="intro-title">Ici bientôt l'intro du scénario</h2>
+);
+
+export default Intro;
---
+++ src/components/Game/index.js
@@ -0,0 +1,30 @@
+import React from 'react';
+import { Switch, Route } from 'react-router-dom';
+
+import Intro from './Intro';
+import Board from './Board';
+import Win from './Win';
+import Loose from './Loose';
+import './game.scss';
+
+const Game = () => (
+  // Ici on switch sur les différentes routes de la game
+  // On passe d'abord par l'intro ensuite par le plateau
+  // et pour finir une page win ou loose
+  <Switch>
+    <Route path="/game" exact>
+      <Intro />
+    </Route>
+    <Route path="/game/board">
+      <Board />
+    </Route>
+    <Route path="/game/win">
+      <Win />
+    </Route>
+    <Route path="/game/loose">
+      <Loose />
+    </Route>
+  </Switch>
+);
+
+export default Game;
