bf2a2841ebd420c67800c97e4044b1535d56ac35
Elenaaab <ellelaby@gmail.com>
2021-08-24 10:24:54 +0200

    update-profil-css

--- src/components/Profil/index.js
+++ src/components/Profil/index.js
@@ -3,6 +3,9 @@ import PropTypes from 'prop-types';
 import Header from 'src/components/Header';
 import Footer from 'src/components/Footer';
 
+import games from 'src/assets/images/games.png';
+
+
 import './profil.scss';
 
 const Profil = ({ email, nickname }) => (
@@ -11,11 +14,12 @@ const Profil = ({ email, nickname }) => (
     <main className="profil-main">
       <div className="profil-container">
         <h2 className="profil-title">Votre profil</h2>
+        <img className="profil-pic" src={games} alt="banniere-img" />
         <ul className="profil-list">
           <li className="profil-list-item">pseudo: {nickname}</li>
           <li className="profil-list-item">email: {email}</li>
         </ul>
-        <a href="#">Changer votre mot de passe</a>
+        <a className="profil-button" href="#">Changer votre mot de passe</a>
       </div>
     </main>
     <Footer />
