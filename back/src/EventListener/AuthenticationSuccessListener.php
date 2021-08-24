60fac98b02d1ab6a959809da0db2f023c3ac0be1
Sacha-HH <sachahardebolle@gmail.com>
2021-08-24 17:08:53 +0200

    Fix AuthenticationSuccessEvent

--- back/src/EventListener/AuthenticationSuccessListener.php
+++ back/src/EventListener/AuthenticationSuccessListener.php
@@ -2,40 +2,31 @@
 
 namespace App\EventListener;
 
-use Symfony\Component\Security\Core\User\UserInterface;
+use App\Entity\User;
 use Lexik\Bundle\JWTAuthenticationBundle\Event\AuthenticationSuccessEvent;
-use Symfony\Component\Serializer\SerializerInterface;
 
 class AuthenticationSuccessListener
 {
-    private $serializer;
-
-    public function __construct(SerializerInterface $serializer)
-    {
-        $this->serializer = $serializer;
-    }
-
 /**
  * @param AuthenticationSuccessEvent $event
  */
     public function onAuthenticationSuccessResponse(AuthenticationSuccessEvent $event)
     {
+        // On récupère le token et l'entité User
         $token = $event->getData();
         $user = $event->getUser();
 
-        if (!$user instanceof UserInterface) {
+        // On vérifie que $user est bien une instance de User
+        if (!$user instanceof User) {
             return;
         }
 
-    //     $token['user'] = array(
-    //     'email' => $user->getUserIdentifier(),
-    //     'roles' => $user->getRoles(),
-    // );
-        $user = $this->serializer->serialize($user, 'json', ['groups' => 'create_user']);
-
-        $token['data'] = [
-            'user' => $user,
-        ];
+        // On ajoute le tableau "user" avec les propriétés à transmettre
+        $token['user'] = array(
+        'id'    => $user->getId(),
+        'email' => $user->getUserIdentifier(),
+        'nickname' => $user->getNickname(),
+    );
 
         $event->setData($token);
     }
