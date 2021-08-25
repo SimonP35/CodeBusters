6178f9ee4d53a0a3c4b3370e6e2d94fc84a5fa1d
Geoffrey-Dct <geoffreydecobert.gd@gmail.com>
2021-08-25 13:52:47 +0200

    css Wip

--- src/components/Home/index.js
+++ src/components/Home/index.js
@@ -5,7 +5,7 @@ import { useHistory } from 'react-router-dom';
 import Header from 'src/components/Header';
 import Footer from 'src/components/Footer';
 // import Loader from 'src/components/Loader';
-import games from 'src/assets/images/games.png';
+import banniere from 'src/assets/images/banniere.png';
 import utils from 'src/utils';
 
 import './home.scss';
@@ -34,22 +34,20 @@ const Home = ({ isLogged, displayErrorMessage }) => {
   return (
     <>
       <Header />
-      <div className="home-page">
-        <main className="home-main">
-          <div className="home-banniere">
-            <img className="home-banniere-img" src={games} alt="banniere-img" />
-          </div>
-          <div className="popular-scenarios-container">
-            {scenarios.map((scenario) => (
-              <div className="popular-scenarios-scenario" key={scenario.id} onClick={() => (startGameCondition())}>
-                <img className="popular-scenarios-scenario-image" src={scenario.image} alt="scenario-img" />
-                <p className="popular-scenarios-scenario-text">SCENARIO X. Viens donc te gâcher le week-end à débugger le code dégueulasse de tonton Maurice !
-                </p>
-              </div>
-            ))}
-          </div>
-        </main>
-      </div>
+      <main className="home-main">
+        <div className="home-banniere">
+          <img className="home-banniere-img" src={banniere} alt="banniere-img" />
+        </div>
+        <div className="popular-scenarios-container">
+          {scenarios.map((scenario) => (
+            <div className="popular-scenarios-scenario" key={scenario.id} onClick={() => (startGameCondition())}>
+              <img className="popular-scenarios-scenario-image" src={scenario.image} alt="scenario-img" />
+              <p className="popular-scenarios-scenario-text">SCENARIO X. Viens donc te gâcher le week-end à débugger le code dégueulasse de tonton Maurice !
+              </p>
+            </div>
+          ))}
+        </div>
+      </main>
       {/* <Loader /> */}
       <Footer />
     </>
