95aba90502c4cbdcd0684c6915914752c97e7339
Elenaaab <ellelaby@gmail.com>
2021-08-29 11:02:39 +0200

    component-bot-setinterval-ok

--- src/components/Game/Bot/index.js
+++ src/components/Game/Bot/index.js
@@ -5,30 +5,45 @@ import utils from 'src/utils';
 
 import './bot.scss';
 
-// Le bot est une image avec une div à l'intérieur qui affiche des messages - avec une transition css qui va bien
-
-// USEEFFECT - SET INTERVAL à mettre en place sur le map d'un tableau d'objets "messages" toutes les 5mn
-
-// Je cherche à incrémenter l'id de l'objet à afficher toutes les x secondes donc ce qu'on peut imaginer c'est : 
-
-// 1. Mettre en place un counter via setinterval, use effect, use state. Toutes les x secondes, il incrémente de 1.
-
-// 2. Il incrémente quoi ? L'id d'un objet de mon tableau.
-
-// 3. Comment j'affiche cet objet ? En faisant un array filter sur l'id de l'objet, qui s'incrémente de 1 toutes les x secondes.
-
+// Custom hook for setInterval :
 // SOURCE INFO :  https://overreacted.io/making-setinterval-declarative-with-react-hooks/
 
 const displayMessages = utils[4].messages;
 
+function useInterval(callback, delay) {
+  const savedCallback = useRef();
+
+  // Remember the latest callback :
+  useEffect(() => {
+    savedCallback.current = callback;
+  }, [callback]);
+
+  // Set up the interval :
+  useEffect(() => {
+    function tick() {
+      savedCallback.current();
+    }
+    if (delay !== null) {
+      const id = setInterval(tick, delay);
+      return () => clearInterval(id);
+    }
+  }, [delay]);
+}
+
 const Bot = () => {
-  const displayMessage = displayMessages.find((message) => message.id === 1);
+  const [idCount, setidCount] = useState(1);
+// UseInterval increments id every 5mn starting at 1 in useState :
+  useInterval(() => {
+    setidCount(idCount + 1);
+  }, 30000); // = 5mn
+  // Array Find display the object matching id incremented :
+  const displayMessage = displayMessages.find((message) => message.id === idCount);
   return (
     <div className="bot-container">
       <img className="bot-pic" src={bot} alt="bot" />
       <div className="bot-message">
         <p className="bot-text-sender">BIG BOSS : </p>
-        <p className="bot-text"> {displayMessage.content}</p>
+        <p className="bot-text" key={displayMessage.id}> {displayMessage.content}</p>
       </div>
     </div>
   );
