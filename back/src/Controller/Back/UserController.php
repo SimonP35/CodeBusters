96619b5a5f583b313651edbbe428b8347005ba55
SimonP35 <simon.pejac@gmail.com>
2021-09-01 11:06:41 +0200

    fix/update-pwd-BO

--- back/src/Controller/Back/UserController.php
+++ back/src/Controller/Back/UserController.php
@@ -87,7 +87,7 @@ class UserController extends AbstractController
 
         if ($form->isSubmitted() && $form->isValid()) {
 
-            $hashedPassword = $passwordHasher->hasher($user, $user->getPassword());
+            $hashedPassword = $passwordHasher->hasher($user, $form->get('password')->getData());
             $user->setPassword($hashedPassword);
             
             $this->getDoctrine()->getManager()->flush();
