d625913533041b7c94418643d06692eb69a31bfa
SimonP35 <simon.pejac@gmail.com>
2021-09-04 23:23:11 +0200

    fix/clean

--- back/src/Controller/Api/GameController.php
+++ back/src/Controller/Api/GameController.php
@@ -12,7 +12,6 @@ use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Component\HttpFoundation\JsonResponse;
-use Symfony\Component\Serializer\SerializerInterface;
 use Symfony\Component\Validator\Validator\ValidatorInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 
