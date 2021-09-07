d6cd764195c52d6b1825aab724f730c5f11a4970
SimonP35 <simon.pejac@gmail.com>
2021-09-07 09:15:55 +0200

    fix/url-reset-pwd

--- back/src/Controller/ResetPasswordController.php
+++ back/src/Controller/ResetPasswordController.php
@@ -116,8 +116,7 @@ class ResetPasswordController extends AbstractController
             // The session is cleaned up after the password has been changed.
             $this->cleanSessionAfterReset();
 
-            return $this->redirectToRoute('http://codebusters.surge.sh/');
-            // return $this->redirectToRoute('app_login');
+            return $this->redirect('http://codebusters.surge.sh/');
         }
 
         return $this->render('reset_password/reset.html.twig', [
